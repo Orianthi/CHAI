436ce52db5569136e206870e4d5a7077a4177c97
chocobr0 <36677071+chocobr0@users.noreply.github.com>
2018-05-23 21:38:02 -0700

    Delete CloseAll.m

--- CloseAll.m
+++
@@ -1,9 +0,0 @@
-function CloseAll
-% Cleanup at end of experiment - Close window, show mouse cursor, close
-% result file, switch Matlab/Octave back to priority 0 -- normal
-% priority:
-
-Screen('CloseAll');
-ShowCursor;
-fclose('all');
-Priority(0);
